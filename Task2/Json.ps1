8af0b3eeaa4c94a6ffafee6ce0723467e412856e
BehbudShikhaliyev <bsixaliyev@gmail.com>
2019-04-05 12:28:02 +0400

    Adding a few parameters

--- Task2/Json.ps1
+++ Task2/Json.ps1
@@ -1,4 +1,10 @@
-$json = Get-Content -Path C:\Users\Behbuds\Desktop\Powershell\Tasks\Task2\Original.json|ConvertFrom-Json
+param(
+    [Parameter(Mandatory = $true)]
+    [string]$PathToJson,
+    [Parameter(Mandatory = $true)]
+    [string]$PathToSave
+)
+$json = Get-Content -Path $PathToJson | ConvertFrom-Json
 $attributes = $json.glossary.GlossDiv.GlossList.GlossEntry
-foreach ($attribute in $attributes) {$attributes.SortAs = 'OMPL'}
-$json |ConvertTo-Json -Depth 100 |Out-File C:\Users\Behbuds\Desktop\Powershell\Tasks\Task2\changed.json
+foreach ($attribute in $attributes) { $attributes.SortAs = 'OMPL' }
+$json | ConvertTo-Json -Depth 100 | Out-File $PathToSave
