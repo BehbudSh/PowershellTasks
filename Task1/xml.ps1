762f73fc499d2384bb605052fc91d2adc9004830
BehbudShikhaliyev <bsixaliyev@gmail.com>
2019-04-05 11:26:28 +0400

    Adding a few parameters

--- Task1/xml.ps1
+++ Task1/xml.ps1
@@ -1,13 +1,17 @@
+param([Parameter(Mandatory = $true)]
+    [String]$PathToXml, 
+    [Parameter(Mandatory = $true)]
+    [String]$PathToSave)
 [int]$i = 0
 while ($i -lt 10) {
     $i++
     $m = $i + $i
-    $xml = [xml](Get-Content .\Xmlcontent.xml)
-    $xml.SelectNodes("//th")| % { 
+    $xml = [xml](Get-Content $PathToXml)
+    $xml.SelectNodes("//th") | ForEach-Object { 
         ($_."#text" = $_."#text".Replace("Title", "Title$i"))
         ($_."#text" = $_."#text".Replace("Artist", "Artist$m"))
     }
-    $xml.Save("C:\Users\Behbuds\Desktop\Powershell\tasks\task1\replaced.xml")
+    $xml.Save("$PathToSave\replaced.xml")
     
-    Copy-Item -Path C:\Users\Behbuds\Desktop\Powershell\tasks\task1\replaced.xml -Destination  C:\Users\Behbuds\Desktop\Powershell\tasks\task1\copied\$i.xml
+    Copy-Item -Path "$PathToSave\replaced.xml" -Destination  "$PathToSave\$i.xml"
 }
