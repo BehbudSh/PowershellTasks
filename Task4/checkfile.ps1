10aaad28ae81287611f7278b234052d3715e3355
BehbudShikhaliyev <bsixaliyev@gmail.com>
2019-04-05 15:12:17 +0400

    Adding a few parameters

--- Task4/checkfile.ps1
+++ Task4/checkfile.ps1
@@ -1,19 +1,19 @@
-﻿#required variables
-
-$printer = "\\printsrv01.atl.lan\Xerox_7830_color -> (City_Point_IT)"
-$path = "C:\Users\Behbuds\Desktop\txtfiles\hellworld.txt"
-$url = "https://drive.google.com/uc?authuser=0&id=1PQExvoqNmGssbm4juVCRfx3v_wGeKvBa&export=download"
-$output = "C:\users\behbuds\Desktop\status\Helloworld.txt"
-$start_time = Get-Date
-
-
- 
+﻿param(
+[Parameter(Mandatory=$true)]
+[string]$Path,
+[Parameter(Mandatory=$true)]
+[string]$Output,
+[Parameter(Mandatory=$true)]
+[string]$Url
+)
+#required variables
+$Printer = "\\printsrv01.atl.lan\Xerox_7830_color -> (City_Point_IT)"
+$StartTime = Get-Date
 #check file system if file exists print it else download it
- 
-if (Test-Path -Path $path ) {Get-Content $path|Out-Printer -Name $printer}
-
-else {Invoke-WebRequest -Uri $url -OutFile $output}
-    
-#load processing time
-
-Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+ if (Test-Path -Path $Path ) { Get-Content $Path | Out-Printer -Name $Printer }
+else { Invoke-WebRequest -Uri $Url -OutFile $Output }
+    #load processing time
+Write-Output "Time taken: $((Get-Date).Subtract($StartTime).Seconds) second(s)"
+# D:\
+# https://drive.google.com/uc?authuser=0&id=1PQExvoqNmGssbm4juVCRfx3v_wGeKvBa&export=download
+# D:\Desktop_Files\Powershell\Task1\PowershellTasks\Task4\HelloWorld.txt
