b2450b21578821400aad04fc2f854a87c634d64d
BehbudShikhaliyev <bsixaliyev@gmail.com>
2019-04-22 10:55:24 +0400

    time uyuyuy

--- Task4/checkfile.ps1
+++ Task4/checkfile.ps1
@@ -12,7 +12,7 @@ $StartTime = Get-Date
 #check file system if file exists print it else download it
 if (Test-Path -Path $Path ) { Get-Content $Path | Out-Printer -Name $Printer }
 else { Invoke-WebRequest -Uri $Url -OutFile $Output }
-#load processing time
+#load processing time uyuyyuy
 Write-Output "Time taken: $((Get-Date).Subtract($StartTime).Seconds) second(s)"
 # D:\
 # https://drive.google.com/uc?authuser=0&id=1PQExvoqNmGssbm4juVCRfx3v_wGeKvBa&export=download
